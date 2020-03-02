80e6055933aae43a83c962cc22a1cecf342c7dd3
Evion Cane <evioncane@gmail.com>
2020-03-02 10:02:39 +0100

    music generation code

---
+++ composing/generation.py
@@ -0,0 +1,209 @@
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+
+# Download and import the MIT 6.S191 package
+import mitdeeplearning as mdl
+import numpy as np
+from tqdm import tqdm
+
+
+# Download the dataset
+songs = mdl.lab1.load_training_data()
+
+# Join our list of song strings into a single string containing all songs
+songs_joined = "\n\n".join(songs)
+
+# Find all unique characters in the joined string
+vocab = sorted(set(songs_joined))
+print("There are", len(vocab), "unique characters in the dataset")
+
+### Define numerical representation of text ###
+# Create a mapping from character to unique index.
+# For example, to get the index of the character "d",
+#   we can evaluate `char2idx["d"]`.
+char2idx = {u:i for i, u in enumerate(vocab)}
+
+# Create a mapping from indices to characters. This is
+#   the inverse of char2idx and allows us to convert back
+#   from unique index to the character in our vocabulary.
+idx2char = np.array(vocab)
+
+
+def vectorize_string(string):
+  vectorized_output = np.array([char2idx[char] for char in string])
+  return vectorized_output
+
+
+vectorized_songs = vectorize_string(songs_joined)
+print ('{} ---- characters mapped to int ----> {}'.format(repr(songs_joined[:10]), vectorized_songs[:10]))
+
+
+### Batch definition to create training examples ###
+def get_batch(vectorized_songs, seq_length, batch_size):
+    # the length of the vectorized songs string
+    n = vectorized_songs.shape[0] - 1
+    # randomly choose the starting indices for the examples in the training batch
+    idx = np.random.choice(n-seq_length, batch_size)
+
+    input_batch = [vectorized_songs[i : i+seq_length] for i in idx]
+
+    output_batch = [vectorized_songs[i+1 : i+seq_length+1] for i in idx]
+
+    # x_batch, y_batch provide the true inputs and targets for network training
+    x_batch = np.reshape(input_batch, [batch_size, seq_length])
+    y_batch = np.reshape(output_batch, [batch_size, seq_length])
+    return x_batch, y_batch
+
+
+x_batch, y_batch = get_batch(vectorized_songs, seq_length=5, batch_size=1)
+
+for i, (input_idx, target_idx) in enumerate(zip(np.squeeze(x_batch), np.squeeze(y_batch))):
+    print("Step {:3d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+
+def LSTM(rnn_units):
+    return tf.keras.layers.LSTM(
+        rnn_units,
+        return_sequences=True,
+        recurrent_initializer='glorot_uniform',
+        recurrent_activation='sigmoid',
+        stateful=True,
+    )
+
+
+### Defining the RNN Model ###
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        # Layer 1: Embedding layer to transform indices into dense vectors
+        #   of a fixed embedding size
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
+
+        # Layer 2: LSTM with `rnn_units` number of units.
+        LSTM(rnn_units),
+
+        # Layer 3: Dense (fully-connected) layer that transforms the LSTM output
+        #   into the vocabulary size.
+        tf.keras.layers.Dense(vocab_size, activation='sigmoid')
+    ])
+
+    return model
+
+
+### Defining the loss function ###
+def compute_loss(labels, logits):
+    loss = tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+    return loss
+
+
+### Hyperparameter setting and optimization ###
+
+# Optimization parameters:
+num_training_iterations = 2000  # Increase this to train longer
+batch_size = 10  # Experiment between 1 and 64
+seq_length = 100  # Experiment between 50 and 500
+learning_rate = 5e-3  # Experiment between 1e-5 and 1e-1
+
+# Model parameters:
+vocab_size = len(vocab)
+embedding_dim = 256
+rnn_units = 1024  # Experiment between 1 and 2048
+
+# Checkpoint location:
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "my_ckpt")
+
+### Define optimizer and training operation ###
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size)
+
+optimizier = tf.keras.optimizers.Adam(
+    learning_rate
+)
+
+@tf.function
+def train_step(x, y):
+    with tf.GradientTape() as tape:
+        y_hat = model(x)
+        loss = compute_loss(y, y_hat)
+
+        # Now, compute the gradients
+        grads = tape.gradient(loss, model.trainable_variables)
+
+        # Apply the gradients to the optimizer so it can update the model accordingly
+        optimizier.apply_gradients(zip(grads, model.trainable_variables))
+        return loss
+
+
+print('##################')
+print('# Begin training!#')
+print('##################')
+
+history = []
+plotter = mdl.util.PeriodicPlotter(sec=2, xlabel='Iterations', ylabel='Loss')
+if hasattr(tqdm, '_instances'): tqdm._instances.clear()  # clear if it exists
+
+for iter in tqdm(range(num_training_iterations)):
+
+    # Grab a batch and propagate it through the network
+    x_batch, y_batch = get_batch(vectorized_songs, seq_length, batch_size)
+    loss = train_step(x_batch, y_batch)
+
+    # Update the progress bar
+    history.append(loss.numpy().mean())
+    plotter.plot(history)
+
+    # Update the model with the changed weights!
+    if iter % 100 == 0:
+        model.save_weights(checkpoint_prefix)
+
+# Save the trained model and the weights
+model.save_weights(checkpoint_prefix)
+
+'''TODO: Rebuild the model using a batch_size=1'''
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+
+# Restore the model weights for the last checkpoint after training
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+model.summary()
+
+
+### Prediction of a generated song ###
+def generate_text(model, start_string, generation_length=1000):
+
+    # Evaluation step (generating ABC text using the learned RNN model)
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    # Empty string to store our results
+    text_generated = []
+
+    # Here batch size == 1
+    model.reset_states()
+    tqdm._instances.clear()
+
+    for i in tqdm(range(generation_length)):
+        predictions = model(input_eval)
+
+        # Remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(idx2char[predicted_id])
+
+    return (start_string + ''.join(text_generated))
+
+
+generated_text = generate_text(model, start_string="X", generation_length=1000)
+
+print('Done!')
+print(generated_text)
